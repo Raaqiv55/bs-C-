57e31fd8ab656de24924794251f9814f09d02fd0
Alamgir55 <alamgir555hossain@gmail.com>
2024-09-23 01:01:08 -0400

    'anonym-namespace'

---
+++ bs/anonym_namespace.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdint>
+
+
+
+namespace {
+    static std::uint64_t faculty(const std::uint8_t n){
+
+        auto result = std::uint64_t{1};
+
+        for(std::uint8_t i = 1; i <= n; i++){
+            result *= i;
+        }
+        return result;
+    }
+}
+
+
+
+int main(){
+    
+    const auto result1 = faculty(5);
+    constexpr auto var = 5 * 3 * 2;
+    constexpr auto ro = faculty(5);
+
+    return 0;
+}
